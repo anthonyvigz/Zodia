539d1325ab81cf349565d881723e58dd83888cec
anthonyvigz <anthonyvigz@gmail.com>
2020-01-12 15:00:57 -0500

    set up register form inputs and styling

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import '../scss/register.scss';
 
 export default function Register() {
 
-  const StyledInput = withStyles({
-    root: {
-      margin: "15px 20px"
-    },
-  })(TextField);
 
   return (
-    <div className="registerPage">
+    <div className="register">
+      <h1>SIGN UP</h1>
       <form className="registerForm" noValidate autoComplete="off">
         <div className="firstLast">
-          <StyledInput id="outlined-basic" label="First Name" variant="outlined" />
-          <StyledInput id="outlined-basic" label="Last Name" variant="outlined" />
+          <input type="text" placeholder="FIRST NAME *" name="firstName" />
+          <input type="text" placeholder="LAST NAME *" name="lastName" />
         </div>
-        <StyledInput id="outlined-basic" label="Email" variant="outlined" />
-        <StyledInput id="outlined-basic" label="Password" variant="outlined" />
+        <input type="text" placeholder="EMAIL *" name="email" />
+        <input type="password" placeholder="PASSWORD *" name="password" />
+        <input type="password" placeholder="CONFIRM PASSWORD *" name="password" />
       </form>
     </div>
   );
