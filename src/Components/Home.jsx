3a9dd03e3695137152ff925354f0d693beb5df87
anthonyvigz <anthonyvigz@gmail.com>
2020-01-12 11:05:38 -0500

    setting up material UI for register page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 
-export default function Home() {
+export default function Home(props) {
+
+  // goes to the Register form page
+  const toRegister = (event) => {
+    event.preventDefault();
+
+    props.history.push('/register')
+  }
 
   return (
     <div>
-        howdy
+        <Button variant="contained" onClick={toRegister} color="primary" size="large">Register</Button>
     </div>
   );
 }
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { findByLabelText } from '@testing-library/react';
+import '../scss/register.scss';
 
 export default function Register() {
 
+  const StyledInput = withStyles({
+    root: {
+      margin: "15px 20px"
+    },
+  })(TextField);
+
   return (
-    <form noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Vigz" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </form>
+    <div className="registerPage">
+      <form className="registerForm" noValidate autoComplete="off">
+        <div className="firstLast">
+          <StyledInput id="outlined-basic" label="First Name" variant="outlined" />
+          <StyledInput id="outlined-basic" label="Last Name" variant="outlined" />
+        </div>
+        <StyledInput id="outlined-basic" label="Email" variant="outlined" />
+        <StyledInput id="outlined-basic" label="Password" variant="outlined" />
+      </form>
+    </div>
   );
 }
