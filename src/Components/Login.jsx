09dab926b1be858e0a44f942609e7d12c234d24c
Anthony Vigliotta <anthonyvigz@gmail.com>
2020-01-15 16:26:12 -0600

    setting up login form

---
+++ src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AbsoluteWrapper from '../Components/AbsoluteWrapper';
+import Error from '../helpers/Error';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const Login = () => {
+    return(
+    <AbsoluteWrapper>
+        <h1>LOGIN</h1>
+    </AbsoluteWrapper>
+    )
+}
+
+export default Login;
