b4479b10ce0c39303ea2b2c17929d13115be539f
anthonyvigz <anthonyvigz@gmail.com>
2020-01-25 20:12:58 -0500

    set up correct spacing for dashboard components

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import SideNav from './SideNav';
+import { makeStyles } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 import '../../scss/dashboard/dashboard.scss';
 
+
+// this sets up the spacing for the dashboard components so it's center
+// within the navigation
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex'
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(8, 4)
+    }
+  }));
+
 function Dashboard(props) {
+
+    const classes = useStyles();
 
     console.log(props)
     return(
         <div className="mainDashboard">
             <SideNav />
-            <h1>hey! {props.firstname}</h1>
-            {/* explicitly declaring child routes for dashboard */}
-            {props.children}
+            <main className={classes.content}>
+                {/* props.children is needed to show all the data in reach router
+                and the protected routes */}
+                <Container>{props.children}</Container>
+            </main>
         </div>
         )
     }
--- src/Components/Dashboard/Overview.jsx
+++ src/Components/Dashboard/Overview.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../../scss/dashboard/overview.scss';
+
 
 export default function Overview() {
     return(
-        <h1>please work</h1>
+        <h1 className="test">please work</h1>
     )
 }
